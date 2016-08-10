dc2fd0c4b023d71bd7c7ff847dffcad23dfd008a
Jonathan Gazeley <jonathan.gazeley@bristol.ac.uk>
2016-08-10 18:17:39 +0100

    Basic manifest

--- manifests/init.pp
+++ manifests/init.pp
@@ -44,5 +44,15 @@
 #
 class percona_toolkit {
 
+  package { 'percona-release':
+    provider => 'rpm',
+    ensure   => 'installed',
+    source   => 'http://www.percona.com/downloads/percona-release/redhat/0.1-3/percona-release-0.1-3.noarch.rpm',
+  }
+
+  package { 'percona-toolkit':
+    ensure  => 'installed',
+    require => Package['percona-release'],
+  }
 
 }
