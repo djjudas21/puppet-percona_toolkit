95e1c9c42274c1f6a849219cb4cc9c6d7786fbd5
Jonathan Gazeley <jonathan.gazeley@bristol.ac.uk>
2016-08-11 13:13:35 +0100

    Limit the module to RHEL

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,16 +4,20 @@
 # Install Percona yum repo and Percona Toolkit
 #
 class percona_toolkit {
+  if $::osfamily == 'RedHat' {
+    # Install Percona yum repo
+    package { 'percona-release':
+      provider => 'rpm',
+      ensure   => 'installed',
+      source   => 'http://www.percona.com/downloads/percona-release/redhat/0.1-3/percona-release-0.1-3.noarch.rpm',
+    }
 
-  package { 'percona-release':
-    provider => 'rpm',
-    ensure   => 'installed',
-    source   => 'http://www.percona.com/downloads/percona-release/redhat/0.1-3/percona-release-0.1-3.noarch.rpm',
+    # Install Percona Toolkit
+    package { 'percona-toolkit':
+      ensure  => 'installed',
+      require => Package['percona-release'],
+    }
+  } else {
+      warning('percona_toolkit module only support on Red Hat compatible distributions')
   }
-
-  package { 'percona-toolkit':
-    ensure  => 'installed',
-    require => Package['percona-release'],
-  }
-
 }
